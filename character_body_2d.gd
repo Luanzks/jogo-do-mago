a678a468cbda4dd73580bebfb3b2b66dd471fecc
Luanzks <luan08112010@gmail.com>
2026-05-18 15:26:49 -0300

    Initial commit

---
+++ character_body_2d.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+var velocidade = Vector2(0,0)
+
+func _physics_process(delta: float) -> void:
+
+	velocidade = Vector2 (0,0)
+	if Input. is_action_pressed("ui_left"):
+		velocidade.x = -250
+		
+	if Input. is_action_pressed("ui_right"):
+		velocidade.x = 250
+		
+	if Input. is_action_pressed("ui_up"):
+		velocidade.y = -250
+		
+	if Input. is_action_pressed("ui_down"):
+		velocidade.y = 250
+		
+	move_and_collide(velocidade * delta)
+	
